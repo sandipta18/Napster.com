737232d38f03f7ab16fbc17036f0eba7e1570166
sandipta18 <sandipta.sardar@innoraft.com>
2023-04-11 21:24:22 +0530

    FT2023-109-Symfony: Implemented Login,Signup,Home page.

--- Napster/src/Controller/MainController.php
+++ Napster/src/Controller/MainController.php
@@ -1,10 +1,9 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Test;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,27 +20,12 @@ class MainController extends AbstractController
    */
   public function LoginController(): Response
   {
-
     return $this->render('home/index.html.twig', [
       'controller_name' => 'UserController',
-      "success_message" => '',
-      "error_message" => ''
     ]);
   }
 
-  /**
-   * SignUpController
-   *
-   * @Route("signup",name="signup")
-   *
-   */
-  public function SignupController(): Response
-  {
-    return $this->render('home/signup.html.twig', [
-      'controller_name' => 'UserController',
-      'error_message' => '',
-    ]);
-  }
+
 
   /**
    * ResetController
@@ -51,21 +35,27 @@ class MainController extends AbstractController
    */
   public function ResetController(): Response
   {
-
     return $this->render('home/reset.html.twig', [
       'controller_name' => 'UserController',
     ]);
   }
 
+
   /**
-   * ValidateController
+   * RegistrationController
    *
    * @Route("registration",name="registration")
+   *
+   * @param Request $rq
+   * @param EntityManagerInterface $entityManager
+   *
+   * @return Response
    *
    */
   public function registration(Request $rq, EntityManagerInterface $entityManager): Response
   {
     $email = $rq->get("email");
+    if($email) {
     $userData = $entityManager->getRepository(Test::class)->findOneBy(["Email" => $_POST['email']]);
     if ($userData == NULL) {
       $userName = $rq->get("username");
@@ -85,28 +75,34 @@ class MainController extends AbstractController
       $test->setPassword($password);
       $test->setGender($gender);
       $test->setCookie($cookie);
+      $test->setImage("https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png");
       $entityManager->persist($test);
       $entityManager->flush();
       return $this->render("home/index.html.twig", [
-        'success_message' => "Signed up succesfully"
+        'success_message' => "Signed up succesfully",
       ]);
     } else {
       return $this->render("home/signup.html.twig", [
         'error_message' => "Account already exists"
       ]);
     }
+  } else {
+      return $this->render('home/signup.html.twig',[
+      ]);
   }
-
+  }
   /**
    *
    * This is used to faciliate login procedure for an user
+   *
    * ValidateLogin Controller
    * @param Request $rq
    * @param EntityManagerInterface $entityManager
    *
-   * @Route("/welcome",name="validatelogin")
+   * @Route("home",name="validatelogin")
    *
    */
+
   public function login(Request $rq, EntityManagerInterface $entityManager): Response
   {
     if ($rq->get('submit_login')) {
@@ -115,13 +111,62 @@ class MainController extends AbstractController
         'Password' => $rq->get('password')
       ]);
       if ($userData != NULL) {
-        return $this->render('home/welcome.html.twig');
+        $encodedValue = base64_encode($userData->getEmail());
+        setcookie("userinfo", $encodedValue, time() + (86400 * 30), "/");
+        return $this->render('home/welcome.html.twig',[
+          'userValue' => $userData
+        ]);
       } else {
         return $this->render("home/index.html.twig", [
-          'success_message' => '',
           'error_message' => "Invalid Credentials"
         ]);
       }
     }
   }
+
+  /**
+   * This function is used to fetch the profile page
+   *
+   * @param Request $rq
+   * @param EntityManagerInterface $entityManager
+   *
+   * @Route("profile",name="profile")
+   *
+   * @return Response
+   *
+   */
+  public function profile(Request $rq,EntityManagerInterface $entityManager):Response
+  {
+    $userData = $_COOKIE['userinfo'];
+    $userData = base64_decode($userData);
+    $userInfo = $entityManager->getRepository(Test::class)->findOneBy([
+    'Email'=>$userData
+    ]);
+    return $this->render('home/profile.html.twig',[
+    'userValue' => $userInfo,
+    ]);
+
+  }
+  /**
+   * @Route("home",name="homepage")
+   */
+  public function redirecti()
+  {
+    return $this->redirectToRoute('homepage');
+  }
+  /**
+   * [Description for signout]
+   *
+   * @param Request $rq
+   * @param EntityManagerInterface $entityManager
+   *
+   * @Route("/signout",name="signout")
+   *
+   */
+  public function signout(Request $rq,EntityManagerInterface $entityManager)
+  {
+     return $this->render('home/index.html.twig',[
+     ]);
+  }
+
 }
